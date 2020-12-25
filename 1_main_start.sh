965c4eebace5d82b232fdd739771a72975c98f96
kokojop <74237836+kokojop@users.noreply.github.com>
2020-12-25 09:58:23 +0900

    Update 1_main_start.sh

--- 1_main_start.sh
+++ 1_main_start.sh
@@ -4,7 +4,7 @@ pkill -9 main_src
 pkill -9 python
 pkill -9 tcpdump
 
-tcpdump -i ens33 port 80 or 8080 -G 300 -w ./7_pcap/log_%Y-%m-%d_%H_%M_%S.pcap -Z root &
+tcpdump -i [ Your network interface name ] port 80 or 8080 -G 300 -w ./7_pcap/log_%Y-%m-%d_%H_%M_%S.pcap -Z root &
 
 cd ./3_c && ./main_src > /dev/null 2>&1 &
 
